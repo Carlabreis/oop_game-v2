9b81af17fe460efecf5f1c26892556a7365c677d
Carla Borges Reis <carlareis.dev.com>
2023-09-28 19:59:55 -0700

    create showMatchedLetter function for phrase class

--- js/Phrase.js
+++ js/Phrase.js
@@ -6,12 +6,13 @@ class Phrase {
   constructor(phrase) {
     this.phrase = phrase.toLowerCase();
   }
+
+  /*
+  adds letter placeholders to the display when the game starts
+  each letter is presented by an empty box, one 'li' element for each letter
+  */
   addPhraseToDisplay() {
-    //adds letter placeholders to the display when the game starts
-    //each letter is presented by an empty box, one 'li' element for each letter
-
     const placeholdersList = document.querySelector("#phrase ul");
-
     const arrayOfCharacters = [...this.phrase];
 
     arrayOfCharacters.forEach((letter) => {
@@ -29,24 +30,27 @@ class Phrase {
     });
   }
 
+  // checks to see if the letter selected by the player matches a letter in the phrase
   checkLetter(selectedLetter) {
-    // checks to see if the letter selected by the player matches a letter in the phrase
     const arrayOfCharacters = [...this.phrase];
-    //if array of charachters include letter selected
-    if (arrayOfCharacters.includes(selectedLetter)){
-        return true;
+
+    if (arrayOfCharacters.includes(selectedLetter)) {
+      this.showMatchedLetter(selectedLetter);
     } else {
-        return false;
+      return false;
     }
-    }
+  }
 
-
-  showMatchedLetter() {
-    /*
-        reveals the letter(s) on the board that matches the player's selection
-        To reveal the matching letter(s), select all of the letter DOM elements that 
-        have a CSS class name that matches the selected letter and replace each 
-        selected element's hide CSS class with the show CSS class.
-        */
+  /*
+  reveals the letter(s) on the board that matches the player's selection.
+  To reveal the matching letter(s), select all of the letter DOM elements that 
+  have a CSS class name that matches the selected letter and replace each 
+  selected element's hide CSS class with the show CSS class.
+  */
+  showMatchedLetter(letter) {
+    const matchingLetters = document.querySelectorAll(`.${letter}`);
+    matchingLetters.forEach(matchingLetter => 
+        matchingLetter.classList.replace('hide', 'show')
+    );
   }
 }
