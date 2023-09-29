c87e99436e5a4a1d444de2a2c8bd6a579ee8870d
Carla Borges Reis <carlareis.dev.com>
2023-09-28 18:52:35 -0700

    create class phrase and first method

--- js/Phrase.js
+++ js/Phrase.js
@@ -1,3 +1,44 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+
+class Phrase {
+  constructor(phrase) {
+    this.phrase = phrase.toLowerCase();
+  }
+  addPhraseToDisplay() {
+    //adds letter placeholders to the display when the game starts
+    //each letter is presented by an empty box, one 'li' element for each letter
+
+    const placeholdersList = document.querySelector("#phrase ul");
+
+    const arrayOfCharacters = [...this.phrase];
+
+    arrayOfCharacters.forEach((letter) => {
+      if (letter !== " ") {
+        const li = document.createElement("li");
+        li.classList.add("hide", "letter", letter);
+        li.innerHTML = letter;
+        placeholdersList.append(li);
+      } else {
+        const li = document.createElement("li");
+        li.classList.add("space");
+        li.innerHTML = " ";
+        placeholdersList.append(li);
+      }
+    });
+  }
+
+  checkLetter() {
+    // checks to see if the letter selected by the player matches a letter in the phrase
+  }
+
+  showMatchedLetter() {
+    /*
+        reveals the letter(s) on the board that matches the player's selection
+        To reveal the matching letter(s), select all of the letter DOM elements that 
+        have a CSS class name that matches the selected letter and replace each 
+        selected element's hide CSS class with the show CSS class.
+        */
+  }
+}
